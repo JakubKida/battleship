b1265eb16f3d9609fc991fc30f7d316352e2af91
Jakub Kida <JakubJKida@gmail.com>
2021-09-10 14:17:51 +0200

    rebuild with favicon and cleanup

--- src/modules/tests/game.test.js
+++
@@ -1,9 +0,0 @@
-import Game from '../factories/game';
-
-describe('Game Factory', () => {
-  test('creating players', () => {
-    const game = new Game('pc');
-    expect(game.playerOne.getType()).toBe('human');
-    expect(game.playerTwo.getType()).toBe('pc');
-  });
-});
